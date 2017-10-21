441c30568e64361f673b855dee61ad7dc604e3db
Igor Churmeev <ingvarch@gmail.com>
2017-10-21 10:53:32 +0300

    Fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['config_maker'],
   scripts=['bin/make_config'],
   version = '0.0.1',
-  description = 'Create config files from environment',
+  description = 'config_maker creates a configuration file in INI format from environment variables',
   author = 'Irina Kharitonova',
   author_email = '',
   url = 'https://github.com/i-kh/make_config_script',
